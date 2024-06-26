48263f723753127bdef6983e33661e362ce77c06
Nathaaaan <devntx@outlook.fr>
2024-06-26 16:40:43 +0200

    Test commit

--- final_project/index.js
+++ final_project/index.js
@@ -5,6 +5,7 @@ const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
+//TEST COMMIT
 
 app.use(express.json());
 
